1410ef02c229535e342ca6ad5a2370345b6d4670
Ricardo González Castillo <rjgcastillo@hotmail.com>
2018-12-07 21:21:38 -0600

    Solve problem 11

---
+++ 11/program.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+
+using namespace std;
+
+#define ALPHABET_SIZE (26)
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+
+struct TrieNode {
+  struct TrieNode *children[ALPHABET_SIZE];
+  bool isWordEnd;
+};
+
+struct TrieNode *getNode(void) {
+  struct TrieNode *pNode = new TrieNode;
+  pNode->isWordEnd = false;
+  for (int i = 0; i < ALPHABET_SIZE; i++) {
+    pNode->children[i] = NULL;
+  }
+  return pNode;
+}
+
+void insert(struct TrieNode *root,  const string key) {
+  struct TrieNode *pCrawl = root;
+  for (int level = 0; level < key.length(); level++) {
+    int index = CHAR_TO_INDEX(key[level]);
+    if (!pCrawl->children[index]) {
+      pCrawl->children[index] = getNode();
+    }
+    pCrawl = pCrawl->children[index];
+  }
+  // mark last node as leaf
+  pCrawl->isWordEnd = true;
+}
+
+bool search(struct TrieNode *root, const string key) {
+  int length = key.length();
+  struct TrieNode *pCrawl = root;
+  for (int level = 0; level < length; level++) {
+    int index = CHAR_TO_INDEX(key[level]);
+    if (!pCrawl->children[index]) return false;
+    pCrawl = pCrawl->children[index];
+  }
+  return (pCrawl != NULL && pCrawl->isWordEnd);
+}
+
+bool isLastNode(struct TrieNode* root) {
+  for (int i = 0; i < ALPHABET_SIZE; i++) {
+    if (root->children[i]) return false;
+  }
+  return true;
+}
+
+void suggestionsRec(struct TrieNode* root, string currPrefix) {
+  if (root->isWordEnd) {
+    cout << currPrefix << endl;
+  }
+  if (isLastNode(root)) return;
+  for (int i = 0; i < ALPHABET_SIZE; i++) {
+    if (root->children[i]) {
+      currPrefix.push_back(97 + i);
+      suggestionsRec(root->children[i], currPrefix);
+    }
+  }
+}
+
+int printAutoSuggestions(TrieNode* root, const string query) {
+  struct TrieNode* pCrawl = root;
+  int level;
+  int n = query.length();
+  for (level = 0; level < n; level++) {
+    int index = CHAR_TO_INDEX(query[level]);
+    if (!pCrawl->children[index]) return 0;
+    pCrawl = pCrawl->children[index];
+  }
+  bool isWord = (pCrawl->isWordEnd == true);
+  bool isLast = isLastNode(pCrawl);
+  if (isWord && isLast) {
+    cout << query << endl;
+    return -1;
+  }
+  if (!isLast) {
+    string prefix = query;
+    suggestionsRec(pCrawl, prefix);
+    return 1;
+  }
+  return 0;
+}
+
+
+int main() {
+  struct TrieNode* root = getNode();
+  insert(root, "hello");
+  insert(root, "dog");
+  insert(root, "hell");
+  insert(root, "cat");
+  insert(root, "a");
+  insert(root, "hel");
+  insert(root, "help");
+  insert(root, "helps");
+  insert(root, "helping");
+  int comp = printAutoSuggestions(root, "hel");
+  if (comp == -1) cout << "No other strings found with this prefix\n";
+  else if (comp == 0) cout << "No string found with this prefix\n";
+  return 0;
+}
